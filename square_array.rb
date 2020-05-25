7e4db0dc4a92c6a547b03246de741b1348d8b4e8
Sonya Gould <sonyavgould@gmail.com>
2020-05-25 18:26:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  squared_numbers = []
+  numbers.each do |i| 
+    squared_numbers << i ** 2
+  end
+   return squared_numbers
 end
+
+# def square_array(numbers)
+# numbers = []
+# new_numbers = #{numbers.collect {|i| i**2 }}
+# end
+# end
