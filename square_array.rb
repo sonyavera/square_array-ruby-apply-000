9cf05a19c606fe3fe81062ed1208cbf684385d27
Sonya Gould <sonyavgould@gmail.com>
2020-05-25 18:17:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(numbers)
   numbers = []
-  new_numbers = numbers.each { |i| numbers << i ** 2 }
-  return new_numbers
+  numbers.each do |i| 
+    new_numbers << i ** 2
+  end
+   new_numbers
 end
 end
 
